a5576bc115d3b8fb78911547c56db32d8eecc489
Jakub Dermek <timmyD@seznam.cz>
2022-05-30 15:02:20 +0200

    main

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import RenderList from "./scripts/renderList";
-import reportWebVitals from "./reportWebVitals";
+//import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from './scripts/store';
 import App from './App';
